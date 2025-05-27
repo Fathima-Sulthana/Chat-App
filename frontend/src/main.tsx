3f1d2a1fa37db9eb8f9e161be0a228219a6205e5
Fathima <fathimanaiha03@gmail.com>
2025-05-27 11:01:16 +0530

    clerk integration success

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const clerkFrontendApi = "pk_test_c2tpbGxlZC1zcXVpcnJlbC00NS5jbGVyay5hY2NvdW50cy5kZXYk"; 
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ClerkProvider publishableKey={clerkFrontendApi}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
+);
+
